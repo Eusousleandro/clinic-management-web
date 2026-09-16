eb8cfc10fe4b5e096392f374a0b4dd24c36b4349
Leandro Sousa Silva <135087138+Eusousleandro@users.noreply.github.com>
2026-09-16 18:24:12 -0300

    Implementando a página de login

---
+++ src/pages/Auth/Login/index.tsx
@@ -0,0 +1,15 @@
+import './style.css'
+
+export default function Login() {
+    return (
+        <div className="content-login">
+            <div className="title-login">
+                <h1>Sejam bem-vindos ao SGC</h1>
+            </div>
+
+            <div className="content-input">
+
+            </div>
+        </div>
+    )
+}
