136626d157fdeec53d5905a46770b23728f2173e
Leandro Sousa Silva <135087138+Eusousleandro@users.noreply.github.com>
2026-09-16 19:31:20 -0300

    Implementando o componente input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,12 @@
+
+import './style.css'
+
+export default function Input() {
+    return (
+        <form action="" className="content-form">
+            <div className="content-input">
+                
+            </div>
+        </form>
+    )
+}
