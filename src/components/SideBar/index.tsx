510d487d6ff6bc823a593952414323e2d53a54aa
Leandro Sousa Silva <135087138+Eusousleandro@users.noreply.github.com>
2026-09-16 19:22:38 -0300

    Implementando a Sidebar

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,22 @@
+
+
+export default function SideBar() {
+    return (
+        <div className="content-sidebar">
+            <div className="siderbar">
+                <ul className="list-sidebar">
+                    <li><a href="" className="items-sidebar">Dashboard</a></li>
+                    <li><a href="" className="items-sidebar">Pacientes</a></li>
+                    <li><a href="" className="items-sidebar">Médicos</a></li>
+                    <li><a href="" className="items-sidebar">Agendamentos</a></li>
+                    <li><a href="" className="items-sidebar">Serviços</a></li>
+                    <li><a href="" className="items-sidebar">Usuários</a></li>
+                </ul>
+                <ul className="list-config-sidebar">
+                    <li><a href="" className="items-config-sidebar">Configurações</a></li>
+                    <li><a href="" className="items-config-sidebar">Sair</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
