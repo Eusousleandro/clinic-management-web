08262b9fd85588da6da4de461a1ec0732147a514
Leandro Sousa Silva <135087138+Eusousleandro@users.noreply.github.com>
2026-09-16 22:11:23 -0300

    Refaturando o código

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -1,6 +1,15 @@
+import { React } from 'react'
+import { Input } from '../components/Input'
 import './style.css'
 
 export default function Login() {
+    const [email, setEmail] = useState<>("")
+    const [password, setPassword] = useState<>("")
+
+    function handlerSubmit() {
+
+    }
+
     return (
         <form action="" className="content-login" onSubmit="">
             <div className="title-login">
@@ -8,7 +17,29 @@ export default function Login() {
             </div>
 
             <div className="content-input">
+                <Input 
+                    type='email'
+                    placeholder='Digite o seu email'
+                    value={email}
+                    onChange={(e) => {
+                        setEmail(e.target.value)
+                    }}
+                />
 
+                <Input
+                    type="password"
+                    placeholder="Digite a sua senha"
+                    value={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value)
+                    }}
+                />
+            </div>
+
+            <div className="buttons">
+                <div className="btn-login">
+                    <Button name="Login" onClick={handlerSubmit}/>
+                </div>
             </div>
         </form>
     )
