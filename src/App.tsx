e990152fc04ac119f4f791ec59518686a825c21d
Leandro Sousa <ordnael.sousass@gmail.com>
2026-09-17 15:08:22 -0300

    Refaturando o codigo

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { NavBar } from './components/NavBar'
 
 export default function App() {
     return (
-        <h1>
+        <>
             <NavBar />
-        </h1>
+        </>
     )
 }
---
+++ src/interfaces/IButton.ts
@@ -0,0 +1,6 @@
+import type { MouseEventHandler } from "react"
+
+export interface IButton {
+    name: string
+    onClick: MouseEventHandler<HTMLButtonElement>
+}
